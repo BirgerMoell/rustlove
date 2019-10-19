b7ce6cd41ee8d99195323fd0309d07c2c65b5753
Birger Moëll <birger.moell@gmail.com>
2019-10-19 16:28:11 +0200

    Added list2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
+import ferret from './ferret.png'
 import './App.css'
+
+let rustLove = [{
+  'name': true
+}]
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={ferret} className='App-logo' alt='logo' />
         <p>
           Things we love about Rust
         </p>
 
+        <a target='_blank' href='https://doc.rust-lang.org/book/'><h2>The book</h2></a>
+        <a target='_blank' href='https://www.rust-lang.org/what/wasm'><h2>Webassembly</h2></a>
+        <a target='_blank' href='https://gitlab.com/solrs/secret-santa'><h2>Secret Santa</h2></a>
+
       </header>
+
     </div>
   )
 }
