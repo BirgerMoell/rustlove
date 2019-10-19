ddfdb9a7b6d0250056d15cf62d9e7ad1e5704f13
Birger Moëll <birger.moell@gmail.com>
2019-10-19 16:21:33 +0200

    Updates

---
+++ precache-manifest.7a4758cbc842d4310bca51ea500fc5fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a815bca8f7992255c815521175f608ed",
+    "url": "/rustlove/index.html"
+  },
+  {
+    "revision": "06d0a4d296514f1619d8",
+    "url": "/rustlove/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "89ea4cab97e51e2770e1",
+    "url": "/rustlove/static/js/2.ab665e5a.chunk.js"
+  },
+  {
+    "revision": "06d0a4d296514f1619d8",
+    "url": "/rustlove/static/js/main.4fa539aa.chunk.js"
+  },
+  {
+    "revision": "27ae3eae7f373fc923fc",
+    "url": "/rustlove/static/js/runtime-main.2290cf33.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/rustlove/static/media/logo.25bf045c.svg"
+  }
+]);
